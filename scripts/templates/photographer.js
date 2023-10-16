f7eea8c86e4c96556819ac7b54f1b55494635dab
Shan Pisciotta <shanpisciotta.pro@gmail.com>
2023-10-16 20:29:02 +0200

    Etape 4 terminée

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,39 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const link = document.createElement( 'a' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const location = document.createElement("h3");
+        const description = document.createElement("p");
+        const pricePerDay = document.createElement("p");
+
+        link.setAttribute("aria-label", name);
+        link.setAttribute("href", `photographer.html?id=${id}`);
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "");
+
+        description.classList.add("description");
+        pricePerDay.classList.add("price");
+
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        location.textContent = `${city}, ${country}`;
+        description.textContent = tagline;
+        pricePerDay.textContent = `${price}€/jour`
+
+        article.appendChild(link);
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(description);
+        article.appendChild(pricePerDay);
+
         return (article);
     }
+
     return { name, picture, getUserCardDOM }
 }
